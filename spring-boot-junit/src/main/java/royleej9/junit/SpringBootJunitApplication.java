0108314eaf39afa733c035d1edcb0b0dbfdda58c
royleej9 <royleej9@gmail.com>
2022-02-13 13:54:47 +0900

    h2

--- spring-boot-junit/src/main/java/royleej9/junit/SpringBootJunitApplication.java
+++ spring-boot-junit/src/main/java/royleej9/junit/SpringBootJunitApplication.java
@@ -10,12 +10,14 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringBootJunitApplication {
 
     public static void main(String[] args) throws Exception {
-        log.info("Before App start- long job for 10s .........................................");
-        Thread.sleep(10 * 1000 * 1);
+        log.info("SpringBootJunitApplication 주석처리");
 
-        log.info("Start App !!!!!!!!!");
+//        log.info("Before App start- long job for 10s .........................................");
+//        Thread.sleep(10 * 1000 * 1);
+//
+//        log.info("Start App !!!!!!!!!");
         SpringApplication.run(SpringBootJunitApplication.class, args);
-        log.info("Started App success!!!!!!!!!");
+//        log.info("Started App success!!!!!!!!!");
     }
 
 }
--- spring-boot-junit/src/main/java/royleej9/junit/web/tarzan/TarzanController.java
+++ spring-boot-junit/src/main/java/royleej9/junit/web/tarzan/TarzanController.java
@@ -10,10 +10,11 @@ import lombok.extern.slf4j.Slf4j;
 public class TarzanController {
 
     public TarzanController() {
-        log.info("INIT TarzanController==============================================================");
-        val count = 100000;
-        for (var i = 0; i < count; i++) {
-            log.info("타잔이" + i * 10 + "원 짜리 팬티를 입고" + i * 20 + "원 짜리 칼을 찾고 노래를 한다.");
-        }
+        log.info("TarzanController 주석처리");
+//        log.info("INIT TarzanController==============================================================");
+//        val count = 100000;
+//        for (var i = 0; i < count; i++) {
+//            log.info("타잔이" + i * 10 + "원 짜리 팬티를 입고" + i * 20 + "원 짜리 칼을 찾고 노래를 한다.");
+//        }
     }
 }
